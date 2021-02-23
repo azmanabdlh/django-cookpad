bf73f421b01e1cde6cd193efb792a48798e5c7fc
azmanabdlh <azmanabdlh@yahoo.com>
2021-02-24 00:22:56 +0900

    change schema model Recipe

--- app/usecases/recipe/models/recipe.py
+++ app/usecases/recipe/models/recipe.py
@@ -1,17 +1,34 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
+from app.usecases.models import User
+
+fs = FileSystemStorage(location=settings.MEDIA_ROOT)
 
 class Recipe(models.Model):
 
   class Meta:
     db_table = 'recipes'
 
+  cover = models.ImageField(
+      storage=fs,
+      blank=False,
+      null=True
+    )
   title = models.CharField(max_length=200, null=False, blank=False)
   description = models.TextField(null=True)
-  cook_long_time = models.CharField(max_length=100, null=True, blank=False)
   serves = models.IntegerField(null=True, blank=False)
   ingredients = models.JSONField(null=True, blank=False)
+  steps = models.JSONField(null=True, blank=False)
+  user = models.ForeignKey(
+      User, 
+      on_delete=models.CASCADE, 
+      null=False
+    )
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True, null=True)
+ 
 
 
   def __str__(self):
