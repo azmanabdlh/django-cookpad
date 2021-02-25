e8a05d67cb865ab59f595a71ca68ac8e9beaf3fe
azmanabdlh <azmanabdlh@yahoo.com>
2021-02-25 12:35:53 +0900

    fix models migrations

--- app/usecases/recipe/models/__init__.py
+++ app/usecases/recipe/models/__init__.py
@@ -1,3 +1,3 @@
-from .recipe_comment import RecipeComment
 from .recipe import Recipe
-from .recipe_step_image import RecipeStepImage
+from .recipe_comment import RecipeComment
+from .recipe_step import RecipeStepImage, RecipeStep
--- app/usecases/recipe/models/recipe_step.py
+++ app/usecases/recipe/models/recipe_step.py
@@ -3,13 +3,29 @@ from django.db import models
 from .recipe import Recipe
 
 class RecipeStep(models.Model):
-	class Meta:
-		db_table = 'recipe_steps'
+	
+  class Meta:
+    db_table = 'recipe_steps'
 
-	description = models.TextField(null=True, blank=False)
-	recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-	created = models.DateTimeField(auto_now_add=True)
-  	updated = models.DateTimeField(auto_now=True, null=True)
+  description = models.TextField(null=True, blank=False)
+  recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True, null=True)
+
+
+class RecipeStepImage(models.Model):
+
+  class Meta:
+    db_table = 'recipe_step_images'
+
+  name = models.CharField(max_length=200)
+  path = models.CharField(max_length=255)
+  size = models.IntegerField(default=0)
+  extension = models.CharField(max_length=200, null=True)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True, null=True)
+
+
 
 
 
